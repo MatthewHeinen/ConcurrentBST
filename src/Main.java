2a90ce2bdfbd2f6822df984bb5c4fa1297cf7e3d
mheinen <mheinen>
2023-05-01 15:05:55 -0400

    Remove method done. Need to make a size method and then the invariance checker next

--- src/Main.java
+++ src/Main.java
@@ -8,19 +8,22 @@ public class Main {
          * Also, should I have two insert functions. One for the single thread and one for the multithread?
          */
         System.out.println("This is a starter project, nothing important in it.");
-        BinarySearchTree<Integer> intTree = new LinkedBinarySearchTree<Integer>(); //Example 1 test
-        intTree.insert(8);
-        intTree.insert(11);
-        intTree.insert(5);
-        intTree.insert(17);
-        intTree.insert(1);
-        intTree.insert(9);
-        intTree.insert(3);
-        System.out.println("Size: " + intTree.size());
-        System.out.println(intTree);
-        intTree.deleteKey(3);
-        System.out.println("Size: " + intTree.size());
-        System.out.println(intTree);
-
+        BST_class bst = new BST_class(); //Example 1 test
+        bst.insert(8);
+        bst.insert(11);
+        bst.insert(5);
+        bst.insert(17);
+        bst.insert(1);
+        bst.insert(9);
+        bst.insert(3);
+        System.out.println("In order printing of a tree with no removes in it: ");
+        bst.inorder();
+        bst.deleteKey(3);
+        bst.deleteKey(11);
+        bst.deleteKey(1);
+        System.out.println("\n");
+        //System.out.println("Size: " + intTree.size());
+        System.out.println("In order printing of a tree with a remove/removes in it: ");
+        bst.inorder();
     }
 }
